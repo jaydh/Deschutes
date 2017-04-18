1517d496da237999a2d8aca392605e2f4058be5b
Jay Howard <jaydh@uw.edu>
2017-04-17 17:21:05 -0700

    Added blogs page

--- src/components/containers/BlogsPage.js
+++ src/components/containers/BlogsPage.js
@@ -2,11 +2,32 @@
 
 import React from 'react';
 import blogs from '../../data/Blogs.js'
+import Slider from 'react-slick'
 
 export default class BlogsPage extends React.Component {
     render() {
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+
         return (
-            <div className ="flex-container">
+            <div className="flex-container">
+                <Slider {...settings}>
+                    {blogs.map(blogItem => {
+                        return (
+                            <div>
+                                <img src={blogItem.photos} height="400" />
+                                <h2>{blogItem.title}</h2>
+                                <h3>by {blogItem.author}     {blogItem.date.toLocaleDateString()}</h3>
+                                <p>{blogItem.text}</p>
+                            </div>
+                        )
+                    })}
+                </Slider>
             </div>
         );
     }
--- src/data/Blogs.js
+++ src/data/Blogs.js
@@ -1,8 +1,16 @@
 const blogs = [
     {
-        'author': '',
-        'content':'',
-        'photos': ''
+        'author': 'jay',
+        'title': 'test1title',
+        date: new Date(2017, 4, 12),
+        'photos': '/img/events/springbbq.jpg',
+        'text': 'test1'
+    }, {
+        'author': 'jay',
+        'title': 'test2title',
+        date: new Date(2017, 4, 13),
+        'photos': '/img/events/colorrun.jpg',
+        'text': 'test2'
     }
 ]
 
