4ccfc437e44f89b678e9a01652795216e61f7228
Jay Howard <jaydh@uw.edu>
2017-05-02 13:39:44 -0700

    Editted staff data

--- src/data/Staff.js
+++ src/data/Staff.js
@@ -28,23 +28,53 @@ const staff = [
     'id': 'hanji-ando',
     'name': 'Hanji',
     'image': 'hanji.png',
-    'introduction': 'He is an international student from Japan studying Business at Bellevue College. He had played baseball for twelve years in Japan. He likes spending time with friends and his two dogs. He usually drinks three cup of coffee everyday.The reason he joined this project because he is one of the international students, and it would have been helpful for him if there is a website like GRS. He hopes this project, GRS, would be a big support for the new international student.'
+    'officer': true,
+    'introduction': 'Hanji is an international student from Japan studying Business at Bellevue College. The most favorite his hobby is baseball that he has played for twelve years. Also, He likes spending time with friends, his two dogs and everyday’s coffee. The most remarkable his experience is working in the multicultural environment. In Japan, he worked with international students from China, Korea, Taiwan, and Iraq. His experience can contribute to make a first step for international students in Seattle. The reason he joined this project is because he is one of the international students, and it would have been helpful for him if there is a website like GRS. His hope from this project, GRS, is new international student could make their time in Seattle better.'
   }, {
     'id': 'yaohan-di',
     'name': 'YaoHan',
     'image': 'daisy.png',
+    'officer': true,
     'introduction': 'Hi, I am Daisy (Yaohan Di), an exchange student from the University of Tokyo. I am a Chinese and went to Japan after I graduated from high school. I major in International relations and I am a junior now. I also went to Okinawa (a beautiful island of Japan) during my high school as an exchange student. Studying abroad can be both exciting and frustrating. Getting used to a new culture and environment and making new friends there are always difficult for international students with the language barrier and cultural differences. Here in Seattle, I fell so lucky to meet the lovely and awesome guys who share the same feeling with me and are also willing to help other international student to get connected in Seattle. I would like to share my experience and help more international students like me, to be their first step to the wonderful life of Seattle. So I joined GRS, hoping we can bring more courage, confidence and opportunities to international students.'
   } , {
     'id': 'Yuki Torikai',
     'name': 'Yuki',
     'image': 'yuki.png',
+    'officer': true,
     'introduction': 'Yuki is an exchange student from Keio University in Tokyo, Japan. She has studied business in her home university and UW. She has also studied Econometrics and has insight into data analysis and economics. She is highly interested in experiential learning. She is always excited working with group of people and learn from them. She has background experience in working for fair-trade business organization, consulting projects and startup company. Some of her skills she&#39;s gained through these experiences are facilitating, critical and logical thinking, forward looking decision making. At free time, she loves cooking, specializing in Japanese food. Now she enjoys learning to cook other cultures&#39; foods, like Korean, Chinese, Thai, Vietnamese and Mexican foods. When you need a shelf at parties, she is always welcome to cook her favorite dishes.'
   } , {
     'id': 'Erisa Steckler',
     'name': 'Erisa',
     'image': 'erisa.png',
-    'introduction': 'hi'
-  }
+    'officer': true,
+    'introduction': 'Erisa is a current student at the University of Washington focusing on International Studies and Japanese. She intends to eventually apply her studies into a career involving diplomatic relations between the United States and Japan. She has previously lived in Yokohama, Japan, but has mainly lived in Seattle for most of her life. On her free time, she enjoys travelling to new places and exploring new hiking destinations.'
+  }, {
+    'id': 'Francis Nyssen',
+    'name': 'Francis',
+    'image': 'francis.png',
+    'officer': true,
+    'introduction': 'Francis was born on the East coast of America in rural Pennsylvania, but was raised in Seattle since the beginning of elementary school (if you have any questions regarding good places to eat, or things to see in the Seattle Area he’s the one to ask). Despite being 19, he loves to travel and to this date has been to 9 countries including China, India, Belize, Guatemala, Italy, France, England, Germany, and Portugal, (10 if you count Canada); and has a bucket list of many more places he would love to go such as Korea, Taiwan, Japan, Greece, Morocco, and Singapore. He is a freshman at UW, bottom of the ladder, and has begun studying international studies through the Jackson School. Francis is a very energetic and ecstatic individual who some might say is a bit eccentric, but nevertheless funny, enjoyable to be around, and quite unpredictable. He is the mood setter, and is almost always the person who brings energy into the room. He is an extremely social individual who has worked hard to expand his social network, and establish connections with officers of many of the cultural clubs around the campus. He loves meeting new people, making new connections, and working with other people, bringing his imagination and fresh mind to the table. Francis has a lot of hobbies, but there are a few things he loves to do more than anything else with his free time. Francis LOVES music, and through that love he also discovered a new passion for dancing (mostly hip hop, popping, and choreographic styles) . He played jazz for 10 years, and listens to many other types of music such as EDM, pop, soundtracks, RNB, and soul. He loves gaming, watching dramas (Korean, Taiwaneses, and'
+  }, {
+    'id': 'Diksha Singh',
+    'name': 'Diksha',
+    'image':'diksha.png',
+    'introduction':'I am Diksha Singh, electrical engineering major at UW Seattle. I am currently in my junior year. I transferred to UW from Bellevue College and it has been amazing journey so far. I love hiking, travelling and dancing apart from studies. I strongly stand for feminism, diversity and humanity.'
+  }, {
+    'id': 'Miu Arai',
+    'name': 'Miu',
+    'image':'miu.png',
+    'introduction':'She is an international student from Japan studying at Bellevue College. Her major in Osaka University is Spanish. She loves sports and travel and has visited almost 20 countries. She hopes international students would have wonderful experiences through GRS.'
+  }, {
+    'id': 'Ichika Kajiwara',
+    'name': 'Ichika',
+    'image':'ichika.png',
+    'introduction':'Hi my name is Ichikawa Kajiwara. I am currently an international student from Ritsumeikan University in Japan. I am not good at expressing my feelings, but through joining this project I would like to change myself and experience many valuable life lesson that I would normally not be able to experience back at home. I believe that this is a wonderful opportunity for me to challenge myself and improve myself for the better as well as make lots of friends.'
+  }, {
+    'id': 'Akemi Nishioka',
+    'name': 'Akemi',
+    'image':'akemi.png',
+    'introduction':'I am an international student from Japan and studying business at BC now. I like getting involved in a new community and making new friends. I hope GRS is going to be your new community which makes your life here more better.'    
+  } 
 ];
 
 export default staff;
