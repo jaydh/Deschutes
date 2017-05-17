89ae84c46acb7613169b734014504c60c148bc82
Jay Howard <jaydh@uw.edu>
2017-05-17 09:20:27 -0700

    Settings changes

--- src/components/containers/ConnectUPage.js
+++ src/components/containers/ConnectUPage.js
@@ -3,10 +3,10 @@
 import React from 'react';
 
 export default class ConnectUPage extends React.Component {
-  render() {
-        return(
+    render() {
+        return (
             <div className="flex-container">
-                <h1>Coming Soon TM</h1>
+                <h1>Coming Soon</h1>
             </div>
         );
     }
